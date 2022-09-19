e316c07430181942189e08e37d13013c44f2b56b
proGabby <inimfonwillie@gmail.com>
2022-09-19 15:38:55 -0700

    taskAdvance

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+
+ * _atoi - Convert a string to integer.
+
+ * @s: char array string
+
+ * Description: Number in the string can be preceded by an infinite
+
+ * number of characters.
+
+ * You need to take into account all -/+ signs before the number.
+
+ * If there are no numbers in the string, return 0.
+
+ * No need to check for overflow.
+
+ * Not allowed to hard-code special values.
+
+ * Return: first integer found in string
+
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int h,p;
+
+	h = 0;
+	p = -1;
+}
