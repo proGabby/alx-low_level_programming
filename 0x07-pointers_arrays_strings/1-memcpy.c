d5ec555570c16dc61b020be39db23238612bd8a6
proGabby <inimfonwillie@gmail.com>
2022-09-26 12:21:30 -0700

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to `dest`
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *yl;
+
+	yl = dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+
+	return (yl);
+}
+
